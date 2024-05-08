5889d59d995b1722822571be0db17585c310da60
박하민 <parkhamint@naver.com>
2024-05-08 23:36:12 +0900

    게시글 삭제 기능 추가

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -57,4 +57,10 @@ public class BoardController {
         model.addAttribute("board", board);
         return "detail";
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
 }
