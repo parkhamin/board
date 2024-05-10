3dbfaf3a18a4b9d68385d53b16c2640ea9aa3641
박하민 <parkhamint@naver.com>
2024-05-10 21:36:47 +0900

    파일 첨부 기능 추가(첨부파일 요청 보내기)

--- src/main/java/com/project/board/dto/BoardDTO.java
+++ src/main/java/com/project/board/dto/BoardDTO.java
@@ -2,6 +2,7 @@ package com.project.board.dto;
 
 import com.project.board.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,10 @@ public class BoardDTO {
     private int boardHits; // 게시글의 조회수
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+    private MultipartFile boardFile; // save.html -> Controller 파일 담는 용도
+    private String originalFileName; // 원본 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
